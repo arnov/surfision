611cd9fc2d317bd411bdfd49c79b72c07b5ac72d
Arno Veenstra <arno.veenstra@company.info>
2020-02-02 16:33:13 +0100

    Option to annotate specific file

--- annotate.py
+++ annotate.py
@@ -1,20 +1,30 @@
 import os
+import click
 from random import shuffle
 
 
-def main():
+def annotate(f):
+    base, ext = f.rsplit('.', 1)
+    output_file = f'{base}.json'
+
+    print(f'Annnotating {f}')
+    if not os.path.exists(output_file):
+        os.system(f'labelme {f} -O {output_file}')
+
+
+@click.command()
+@click.argument('image_path', type=click.Path(), required=False)
+def main(image_path=None):
+    if image_path:
+        annotate(image_path)
+        return
+
     dir = 'data'
     files = os.listdir(dir)
     shuffle(files)
 
     for f in files:
-        base, ext = f.rsplit('.', 1)
-        input_file = f'{dir}/{f}'
-        output_file = f'{dir}/{base}.json'
-
-        print(f'Annnotating {input_file}')
-        if not os.path.exists(output_file):
-            os.system(f'labelme {input_file} -O {output_file}')
+        annotate(dir + '/' + f)
 
 
 if __name__ == '__main__':
