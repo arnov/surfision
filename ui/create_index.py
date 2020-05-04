5aac6db5062967af6aabe933a75c28313bbb0494
Arno Veenstra <arno.veenstra@company.info>
2020-05-04 22:36:56 +0200

    Add cams in ui

--- ui/create_index.py
+++ ui/create_index.py
@@ -1,5 +1,6 @@
 from fetch.fetch_data import ALL_CAMS
 from fetch.cams import ChunkListCam, YoutubeCam
+import urllib.parse
 
 
 for cam_id, cam in ALL_CAMS.items():
@@ -13,12 +14,18 @@ for cam_id, cam in ALL_CAMS.items():
         )
         print(html)
     elif isinstance(cam, ChunkListCam):
+        video_list_url = cam.video_list_url
+
+        if not cam.cors_allowed:
+            get_param = urllib.parse.quote(cam.video_list_url)
+            video_list_url = f'http://35.196.30.60:8080?url={get_param}'
+
         html = (
             '<div>'
               f'<h1>{cam.name}</h1>'
               f'<div id="player_{cam_id}"></div>'
               '<script>'
-                f'var player2 = new Clappr.Player({{source: "{cam.video_list_url}", parentId: "#player_{cam_id}"}});'
+                f'var player_{cam_id} = new Clappr.Player({{source: "{video_list_url}", parentId: "#player_{cam_id}", mimeType: "application/vnd.apple.mpegurl"}});'
               '</script>'
             '</div>'
         )
