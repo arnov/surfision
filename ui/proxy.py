52d81272de4fc0d927eafe7660ceffe658cb9594
Arno Veenstra <arno.veenstra@company.info>
2020-05-05 10:45:49 +0200

    Serve index.html as well

--- ui/proxy.py
+++ ui/proxy.py
@@ -1,11 +1,19 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, abort
 import requests
+import urllib.parse
 
 from fetch.cams import ChunkListCam
 
 app = Flask(__name__)
 
 URL_LOOKUP = {}
+ALLOWED_HOSTS = {'cams.cdn-surfline.com'}
+
+
+@app.route('/index.html')
+def index():
+    with open('ui/index.html') as f:
+        return ''.join(f.readlines())
 
 
 @app.route('/', defaults={'u_path': ''})
@@ -15,9 +23,14 @@ def proxy(u_path):
 
     if not url:
         vid_id = u_path.strip('/')
-        url = URL_LOOKUP[vid_id]
+        url = URL_LOOKUP.get(vid_id)
+        if not url:
+            abort(404)
 
-    res = requests.get(url)
+    netloc = urllib.parse.urlparse(url).netloc
+
+    if netloc in ALLOWED_HOSTS:
+        res = requests.get(url)
 
     if '.m3u8' in url:
         video_ids = ChunkListCam.parse_video_list(res)
